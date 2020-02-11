30cf66e8df53ff0046d4665817b1c4012c691e48
ririmrcl <marcelinariani@gmail.com>
2020-02-11 13:55:21 +0700

    pertama

---
+++ resources/views/About.blade.php
@@ -0,0 +1,15 @@
+@extends('layout/main')
+
+@section('Tittle', 'RIRIMRCL')
+
+@section('container')
+    <div class="container">
+        <div class="row">
+            <div class="col-10">
+                <h1 class="mt-3">About page</h1>
+            </div>
+        </div>
+    </div>
+@endsection
+
+ 
